b1f982af4fd5bff6d70712e3f603d8168c229ce7
RaiTech 💻 <156493050+Rai567245@users.noreply.github.com>
2026-01-30 23:41:45 +0800

    Add files via upload

---
+++ python-strings/python-concatenate-strings.py
@@ -0,0 +1,8 @@
+# This program demonstrates the use of concatenation to combine multiple strings into one.
+# It shows how to concatenate strings using the '+' operator and formatted strings (f-strings).
+
+# Concatenation using the '+' operator
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print("Full Name using '+':", full_name)
---
+++ python-strings/python-format-strings-sample.py
@@ -0,0 +1,14 @@
+# This program demonstrates the use of f-strings for formatting strings in Python.
+
+#F-String Example
+first_name = "John"
+last_name = "Doe"
+full_name = f"{first_name} + {last_name}"
+print(full_name)
+
+# Plaveholder Variables
+name = "Alice"
+age = 30
+nameAgeInfo = f"{name} is {age} years old."
+print(nameAgeInfo)
+
